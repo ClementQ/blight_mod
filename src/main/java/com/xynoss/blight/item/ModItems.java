d1643b35ed347b0e2e4a39f885f16032ce8aee20
Clément <clement.quesnel@outlook.com>
2025-03-27 11:42:08 +0100

    Add a complex item that chisel block into in other

--- src/main/java/com/xynoss/blight/item/ModItems.java
+++ src/main/java/com/xynoss/blight/item/ModItems.java
@@ -1,4 +1,5 @@
 package com.xynoss.blight.item;
+import com.xynoss.blight.item.custom.ChiselItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import com.xynoss.blight.Blight;
 import net.minecraft.item.Item;
@@ -13,8 +14,11 @@ public class ModItems {
 
 public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Blight.MOD_ID,"pink_garnet")))));
 public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Blight.MOD_ID,"raw_pink_garnet")))));
+
 public static final Item BLIGHT_INGOT = registerItem("blight_ingot", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Blight.MOD_ID,"blight_ingot")))));
 public static final Item RAW_BLIGHT = registerItem("raw_blight", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Blight.MOD_ID,"raw_blight")))));
+
+public static final Item CHISEL_ITEM = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32).registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Blight.MOD_ID,"chisel")))));
 
 private static Item registerItem(String name, Item item) {
     return Registry.register(Registries.ITEM, Identifier.of(Blight.MOD_ID, name), item);
